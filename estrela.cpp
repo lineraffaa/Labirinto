67fcd836437a9d56e0e6ac84bf4e85a6bfb4d463
Line <91154842+lineraffaa@users.noreply.github.com>
2026-04-19 11:02:42 -0300

    Atualizado

--- estrela.cpp
+++ estrela.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iomanip>
-#define TAM 100
+#define TAM 4
 #include <cstdlib>
 #include <ctime>
 #include <vector>
@@ -19,28 +19,42 @@ void estrela(float matriz[TAM][TAM], int iniciox, int inicioy, int fimx, int fim
 
     bool listafechada[TAM][TAM] = {false};
 
+    //* aqui esta o nó inicial
     No inicial = {iniciox, inicioy, 0, 0, 0, -1, -1};
+
+    //!
     inicial.h = abs(iniciox - fimx) + abs(inicioy - fimy);
+
+    //! função heuristica
     inicial.f = inicial.g + inicial.h;
+
     listaaberta.push_back(inicial);
 
     int paix[TAM][TAM];
     int paiy[TAM][TAM];
-    
 
-
-    //! para representar as direções é utilizado dois vetores 
-    int direx[] = {-1,1,0,0}, direy[] = {0,0,-1,1};
-    
+    //! para representar as direções é utilizado dois vetores
+    int direx[] = {-1, 1, 0, 0}, direy[] = {0, 0, -1, 1};
+    for (int i = 0; i < TAM; i++) {
+    for (int j = 0; j < TAM; j++) {
+        paix[i][j] = -1;
+        paiy[i][j] = -1;
+    }
+}
 
     while (!listaaberta.empty())
     {
+
         int idx = 0;
+
         for (int i = 0; i < listaaberta.size(); i++)
         {
+
             if (listaaberta[i].f < listaaberta[idx].f)
             {
                 idx = i;
+
+                cout << idx << "  encontrar o menor no da lista e atualizar o indice \n";
             }
         }
         No atual = listaaberta[idx];
@@ -53,75 +67,95 @@ void estrela(float matriz[TAM][TAM], int iniciox, int inicioy, int fimx, int fim
 
         paiy[atual.x][atual.y] = atual.paiY;
 
-
-           cout <<fimx << fimy;
+        //   cout << fimx << fimy;
+        cout <<"X" << atual.x << "Y" <<atual.y << endl;
 
         if (atual.x == fimx && atual.y == fimy)
         {
+            cout << "chegou no destino!" << endl;
+
             int endx = fimx;
             int endy = fimy;
-         
+           
             while (endx != -1 && endy != -1)
             {
-                    
-                 cout << "(" << endx << ", " << endy << ")" << endl;
+
+                cout << "(" << endx << ", " << endy << ")" << endl;
                 int px = paix[endx][endy];
                 int py = paiy[endx][endy];
                 endx = px;
                 endy = py;
-
-                
             }
-            break;
-
-
+             break;
         }
 
-        for(int j = 0; j < 4; j++){
-            int coorx, coory; 
-            coorx = atual.x + direx[j]; 
-            coory = atual.y + direy[j];
-            
-            if(coorx < 0 || coorx >= TAM || coory < 0 || coory >= TAM) continue;
+        for (int j = 0; j < 4; j++)
+        {
 
-           if(matriz[coorx][coory] == 1) continue;
-            if(listafechada[coorx][coory]) continue;
+            int coorx, coory;
+
+            bool existe = false;
+
+            coorx = atual.x + direx[j];
+            coory = atual.y + direy[j];
+
+            if (coorx < 0 || coorx >= TAM || coory < 0 || coory >= TAM)
+                continue;
+
+            if (matriz[coorx][coory] == 1)
+                continue;
+            if (listafechada[coorx][coory])
+                continue;
 
             float novoG = atual.g + 1;
             float novoH = abs(coorx - fimx) + abs(coory - fimy);
             float novoF = novoG + novoH;
 
             No vizinho = {coorx, coory, novoF, novoG, novoH, atual.x, atual.y};
-            listaaberta.push_back(vizinho);
-        
+            //* antes de adicionar verificar se tem na lista
+            for (int i = 0; i < listaaberta.size(); i++)
+            {
+                if (listaaberta[i].x == coorx && listaaberta[i].y == coory)
+                {
+                    if (novoG < listaaberta[i].g)
+                    {
+                        listaaberta[i].g = novoG;
+                        listaaberta[i].f = novoF;
+                        listaaberta[i].paiX = atual.x;
+                        listaaberta[i].paiY = atual.y;
+                    }
+                    existe = true;
+                    break;
+                }
+            }
+            if (!existe)
+            {
+                listaaberta.push_back(vizinho);
+            }
+
+            cout << "vizinho adicionado: (" << coorx << ", " << coory << ")" << endl;
         }
-        
-
     }
-
-
-   
-
+    
 }
 
 int main()
 {
 
-    float matriz[TAM][TAM];
-
     srand(time(NULL));
 
     // Preenchendo a matriz
+    float matriz[TAM][TAM];
+    // no main, antes de chamar estrela
     for (int i = 0; i < TAM; i++)
     {
         for (int j = 0; j < TAM; j++)
         {
-
             matriz[i][j] = rand() % 2;
+            cout << matriz[i][j] << " ";
         }
+        cout << endl;
     }
-    matriz[0][0] = 0;
-    matriz[TAM-1][TAM-1] = 0;
-    cout << "AQQ";
-   estrela(matriz, 0, 0, TAM - 1, TAM - 1);
+
+    estrela(matriz, 0, 0, TAM - 1, TAM - 1);
 }
